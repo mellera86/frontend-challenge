47cab52cc563517e3b9728fc68b17402f1b6bd62
Matias Ellera <elleramatias@gmail.com>
2023-07-13 16:47:58 -0400

    email validation

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -79,11 +79,11 @@ const validateForm = (values) => {
   if (!values.email) {
     errors.email = 'Email is required';
   }
-  // else if (
-  //   !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-  // ) {
-  //   errors.email = 'Invalid email address';
-  // }
+  else if (
+    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+  ) {
+    errors.email = 'Invalid email address';
+  }
   if (!values.password) {
     errors.password = 'Password is required';
   }
